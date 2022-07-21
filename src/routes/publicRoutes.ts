af8574cada5fff7adb19c77592b181506c6575a7
ChrisCoy <christopherleegs@outlook.com>
2022-07-21 02:48:06 -0300

    fix deploy

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -11,42 +11,40 @@ interface IRequestValidate extends Request {
 }
 
 publicRouter.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email: email });
-  if (!user) {
-    res.status(400).json({ err: "Invalid Credentials!" });
-    return;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      res.status(400).json({ err: "Invalid Credentials!" });
+      return;
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      res.status(400).json({ err: "Invalid Credentials!" });
+      return;
+    }
+
+    const accessToken = createTokens(user);
+
+    res.cookie("access-token", accessToken, {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
+    });
+  } catch (err) {
+    return res.status(400).json({ err });
   }
 
-  const match = await bcrypt.compare(password, user.password);
-  if (!match) {
-    res.status(400).json({ err: "Invalid Credentials!" });
-    return;
-  }
-
-  const accessToken = createTokens(user);
-
-  res.cookie("access-token", accessToken, {
-    maxAge: 1000 * 60 * 60 * 24,
-    httpOnly: false,
-  });
-
-  console.log(accessToken);
-
-  console.log("Cookies: ", req.cookies);
-
-  // Cookies that have been signed
-  console.log("Signed Cookies: ", req.signedCookies);
-
-  res.status(200).send();
+  return res.status(200).send();
 });
 
 publicRouter.post("/validate", validateToken, (req: IRequestValidate, res) => {
   if (req.authenticated) {
     return res.status(200).send();
   }
-
 
   console.log("Cookies: ", req.cookies);
 
